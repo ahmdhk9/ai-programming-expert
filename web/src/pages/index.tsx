6d7922a0b19ada941e0036268fad1e0ab223e937
ahmdhk9 <ahmdhk9@users.noreply.github.com>
2025-11-24 01:53:57 +0300

    Create index.tsx

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ display: "flex", height: "100vh", alignItems: "center", justifyContent: "center" }}>
+      <h1>Hello, Agent 👋</h1>
+    </div>
+  );
+}
