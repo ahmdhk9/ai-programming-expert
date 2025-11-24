12feb476b85e82bf7a1a4c805338569edefa5213
ahmdhk9 <ahmdhk9@users.noreply.github.com>
2025-11-24 09:06:04 +0300

    Update agent.ts

--- web/src/pages/api/agent.ts
+++ web/src/pages/api/agent.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { db } from "../../lib/firebase";
+import { db } from "../../../lib/firebase";
 import { collection, addDoc } from "firebase/firestore";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
